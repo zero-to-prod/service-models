d8a00bc3da8ae3298a96c0c9ae98556f6e754cf9
david <61474950+zero-to-prod@users.noreply.github.com>
2023-12-01 09:12:31 -0500

    Feat/cast to array of enums (#16)
    * Add support for an array of enums in ServiceModel.php
    
    * Add tests.
    
    * Update README.md
    
    ---------
    
    Co-authored-by: david_smith <david_smith@sweetwater.com>

--- tests/Unit/CastToArrayTest.php
+++ tests/Unit/CastToArrayTest.php
@@ -1,17 +1,31 @@
 <?php
 
 use Zerotoprod\AppServiceModel\Tests\Models\Child;
+use Zerotoprod\AppServiceModel\Tests\Models\MockEnumCast;
 use Zerotoprod\AppServiceModel\Tests\Models\TopLevelCastArray;
+use Zerotoprod\AppServiceModel\Tests\Models\TopLevelCastArrayEnums;
 
 test('top level cast', function () {
-    $TopLevelCast = TopLevelCastArray::make([
+    $TopLevelCastArray = TopLevelCastArray::make([
         TopLevelCastArray::children => [
             [Child::name => 'name'],
             [Child::name => 'name'],
         ]
     ]);
 
-    expect($TopLevelCast->children)->toBeArray()
-        ->and($TopLevelCast->children[0]->name)->toBe('name')
-        ->and($TopLevelCast->children[0])->toBeInstanceOf(Child::class);
+    expect($TopLevelCastArray->children)->toBeArray()
+        ->and($TopLevelCastArray->children[0]->name)->toBe('name')
+        ->and($TopLevelCastArray->children[0])->toBeInstanceOf(Child::class);
+});
+test('top level cast to enums', function () {
+    $TopLevelCastArrayEnums = TopLevelCastArrayEnums::make([
+        TopLevelCastArrayEnums::children => [
+            MockEnumCast::first->value,
+            MockEnumCast::second->value,
+        ]
+    ]);
+
+    expect($TopLevelCastArrayEnums->children)->toBeArray()
+        ->and($TopLevelCastArrayEnums->children[0]->name)->toBe('first')
+        ->and($TopLevelCastArrayEnums->children[0])->toBeInstanceOf(MockEnumCast::class);
 });
