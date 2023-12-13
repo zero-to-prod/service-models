05a64e30597acd4dade20c6ef3c4f169cee7a57b
david_smith <david_smith@sweetwater.com>
2023-12-13 11:38:52 -0500

    Remove unused models from NestedChildTest
    The unused models ChildWithoutTrait and TopLevelWithoutChildTrait have been removed from the NestedChildTest. These models were previously involved in a test that expected a TypeError, which has also been removed because the models are no longer needed. This results in cleaner test code and additional clarity about what models and features are being tested.

--- tests/Unit/NestedChildTest.php
+++ tests/Unit/NestedChildTest.php
@@ -1,9 +1,7 @@
 <?php
 
 use Zerotoprod\AppServiceModel\Tests\Models\Child;
-use Zerotoprod\AppServiceModel\Tests\Models\ChildWithoutTrait;
 use Zerotoprod\AppServiceModel\Tests\Models\TopLevel;
-use Zerotoprod\AppServiceModel\Tests\Models\TopLevelWithoutChildTrait;
 
 test('accesses child service model', function () {
     $TopLevel = TopLevel::make([
@@ -15,11 +13,3 @@ test('accesses child service model', function () {
     expect($TopLevel->child)->toBeInstanceOf(Child::class)
         ->and($TopLevel->child->name)->toBe('name');
 });
-
-test('type error service model without trait', function () {
-    TopLevelWithoutChildTrait::make([
-        TopLevelWithoutChildTrait::child => [
-            ChildWithoutTrait::name => 'name'
-        ]
-    ]);
-})->throws(TypeError::class);
