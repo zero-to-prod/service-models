ff7e7a09f65e5677882835c52fc7662f8a73b125
david_smith <david_smith@sweetwater.com>
2023-12-13 11:37:00 -0500

    Remove DateTimeModel test files
    The DateTimeModel and associated unit test files are deleted as part of a refactoring process. This refactor aims to improve the handling of enums and classes within the ServiceModel trait. With the new implementation, data casting to various classes will be possible.

--- tests/Unit/DateTimeTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Zerotoprod\AppServiceModel\Tests\Models\DateTimeModel;
-
-test('top level value cast', function () {
-    expect(DateTimeModel::make([DateTimeModel::time => '2021-01-01 00:00:00'])->time)
-        ->toBeInstanceOf(DateTime::class);
-});
