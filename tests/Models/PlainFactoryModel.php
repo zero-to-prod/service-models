1528b58482c2862afe2e5bc9b73828dd57d63e39
david <61474950+zero-to-prod@users.noreply.github.com>
2023-12-20 12:04:58 -0500

    Chore/update tests for plain factory support (#51)
    * Update factory class in PlainFactoryModel
    
    The factory class in PlainFactoryModel has been updated from FactoryModelFactory to PlainFactoryModelFactory. This change aligns the model with the corresponding factory class, ensuring consistency and reducing potential confusion.
    
    * Add PlainFactoryModelFactory test class
    
    A new test class, PlainFactoryModelFactory, has been added under the Zerotoprod\AppServiceModel\Tests\Models namespace. It replaces the former FactoryModelFactory. The new class also includes a method for setting the name, enhancing versatility in tests.
    
    ---------
    
    Co-authored-by: david_smith <david_smith@sweetwater.com>

--- tests/Models/PlainFactoryModel.php
+++ tests/Models/PlainFactoryModel.php
@@ -8,7 +8,7 @@ class PlainFactoryModel
 {
     use HasFactory;
 
-    public static string $factory = FactoryModelFactory::class;
+    public static string $factory = PlainFactoryModelFactory::class;
 
     public const name = 'name';
     public const Child = 'Child';
