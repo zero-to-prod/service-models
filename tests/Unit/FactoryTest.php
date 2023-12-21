545847cf84f6c37022b8aae79578919f3bfbf21c
david_smith <david_smith@sweetwater.com>
2023-12-20 19:08:45 -0500

    Add additional tests to FactoryTest.php
    Expand the scope of FactoryTest.php by including tests for expanding attributes and the default definition of a factory. These tests will help identify any errors that may arise when expanding the attributes of an object or when creating default factory objects.

--- tests/Unit/FactoryTest.php
+++ tests/Unit/FactoryTest.php
@@ -2,6 +2,7 @@
 
 use Zerotoprod\AppServiceModel\Tests\Models\FactoryModel;
 use Zerotoprod\AppServiceModel\Tests\Models\FactoryModelChild;
+use Zerotoprod\ServiceModel\Factory;
 
 test('definition', function () {
     $FactoryModel = FactoryModel::factory()->make();
@@ -47,4 +48,31 @@ test('overridden make', function () {
         ]);
 
     expect($FactoryModel->name)->toBe('name');
+});
+it('expand attributes', function () {
+    $factory = new Factory;
+
+    // Define a callable (not a string or array)
+    $callableAttribute = function ($definition) {
+        return count($definition) * 2;
+    };
+
+    // Invoke method via reflection as it's a protected method
+    $reflection = new ReflectionClass(get_class($factory));
+    $method = $reflection->getMethod('expandAttributes');
+    $method->setAccessible(true);
+
+    // Define a definition
+    $definition = ['attribute' => $callableAttribute];
+
+    // Invoke the method
+    $result = $method->invokeArgs($factory, [$definition]);
+
+    // Check if callable is called and calculated correct result
+    expect($result)->toBeArray()
+        ->and($result['attribute'])->toBe(2);
+});
+it('returns empty array for default definition of Factory', function () {
+    $factory = new Factory();
+    expect($factory->definition())->toBeArray()->toBeEmpty();
 });
