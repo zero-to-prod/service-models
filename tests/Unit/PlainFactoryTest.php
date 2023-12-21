7ea2c1a131794bcc846961b629a7bd4bb304b1db
david_smith <david_smith@sweetwater.com>
2023-12-20 19:38:25 -0500

    Add new test for definition override in PlainFactoryModel
    This commit adds a new test in PlainFactoryModel to check for the 'definition override' function. It ensures that the 'setName' method can successfully update the 'name' attribute to a new value, in this case, 'test'. This additional test enhances the unit tests and ensures the function works properly.

--- tests/Unit/PlainFactoryTest.php
+++ tests/Unit/PlainFactoryTest.php
@@ -6,3 +6,8 @@ test('definition', function () {
     expect(PlainFactoryModel::factory()->make()->attributes['name'])
         ->toBe('definition');
 });
+
+test('definition override', function () {
+    expect(PlainFactoryModel::factory()->setName('test')->make()->attributes['name'])
+        ->toBe('test');
+});
