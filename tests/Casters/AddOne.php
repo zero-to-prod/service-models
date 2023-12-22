e8fb4e57f30be6ccd03de5c745d782fb7ebd7bd2
david_smith <david_smith@sweetwater.com>
2023-12-22 11:58:33 -0500

    Refactor argument name in parse function
    The argument name in parse function of AddOne class has been changed from $property_values to $values. This offers a more generalized nomenclature, enhancing readability and providing a more concise understanding of what type of values the function is expected to handle.

--- tests/Casters/AddOne.php
+++ tests/Casters/AddOne.php
@@ -6,8 +6,8 @@ use Zerotoprod\ServiceModel\Contracts\CanParse;
 
 class AddOne implements CanParse
 {
-    public function parse(array $property_values): int
+    public function parse(array $values): int
     {
-        return $property_values[0] + 1;
+        return $values[0] + 1;
     }
 }
