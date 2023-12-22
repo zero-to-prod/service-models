1ef8e10397602a55e161674b4bd925450ba6536b
david_smith <david_smith@sweetwater.com>
2023-12-22 11:37:25 -0500

    Implement 'CanParse' interface in 'Cast' class
    The 'Cast' class now implements the 'CanParse' interface, adding a 'parse' method that utilizes the class specified to parse values. This new implementation improves type parsing flexibility and ensures safer type conversions. Changes in the code structure provide better handling for unexpected exceptions during the casting process.

--- src/Attributes/Cast.php
+++ src/Attributes/Cast.php
@@ -1,14 +1,19 @@
 <?php
-// @codeCoverageIgnoreStart
+
 namespace Zerotoprod\ServiceModel\Attributes;
 
 use Attribute;
+use Zerotoprod\ServiceModel\Contracts\CanParse;
 
 #[Attribute]
-class Cast
+class Cast implements CanParse
 {
     public function __construct(public readonly string $class)
     {
     }
+
+    public function parse(array $values)
+    {
+        return (new $this->class)->parse($values);
+    }
 }
-// @codeCoverageIgnoreEnd
