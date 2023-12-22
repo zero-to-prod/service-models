0faac25cf9633e981e7f5958055ed38254e6cf81
david_smith <david_smith@sweetwater.com>
2023-12-22 11:36:39 -0500

    Add CustomCasterTest unit test
    A new test file, CustomCasterTest.php, has been added for handling unit testing of custom casting operations. This file contains two tests, one for each kind of custom casting, demonstrating the correct parsing of numeric values. This inclusion strengthens the test coverage of the application.

---
+++ tests/Unit/CustomCasterTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Zerotoprod\AppServiceModel\Tests\Models\CustomCastClass;
+
+test('invokes custom caster', function () {
+    $ValueCast = CustomCastClass::make([
+        CustomCastClass::add_one => 1
+    ]);
+    expect($ValueCast->add_one)->toBeInt()
+        ->and($ValueCast->add_one)->toBe(2);
+});
+
+test('invokes custom value caster', function () {
+    $ValueCast = CustomCastClass::make([
+        CustomCastClass::add_two => 1
+    ]);
+    expect($ValueCast->add_two)->toBeInt()
+        ->and($ValueCast->add_two)->toBe(4);
+});
