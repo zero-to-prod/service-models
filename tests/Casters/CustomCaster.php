3e068c15dc0d489dcc56b388c470440f092ea82d
david_smith <david_smith@sweetwater.com>
2023-12-22 11:58:42 -0500

    Refactor argument name in CustomCaster class
    The argument name in the constructor of CustomCaster class has been renamed from $value to $attribute_constructor_value. This change provides a clearer understanding of the role of the argument within the scope of the constructor. It also improves code readability by providing more specific context for the argument usage.

--- tests/Casters/CustomCaster.php
+++ tests/Casters/CustomCaster.php
@@ -8,17 +8,16 @@ namespace Zerotoprod\AppServiceModel\Tests\Casters;
 
 use Attribute;
 use Zerotoprod\ServiceModel\Contracts\CanParse;
-use function Zerotoprod\ServiceModel\Attributes\enum_exists;
 
 #[Attribute]
 class CustomCaster implements CanParse
 {
-    public function __construct(public readonly int $value)
+    public function __construct(public readonly int $attribute_constructor_value)
     {
     }
 
     public function parse(array $values): int
     {
-        return $values[0] + $this->value;
+        return $values[0] + $this->attribute_constructor_value;
     }
 }
