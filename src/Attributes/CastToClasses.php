ee0a57b15be3a029a5de03edad7548fb9794fd5b
david_smith <david_smith@sweetwater.com>
2023-12-23 21:51:41 -0500

    Mark CastToClasses class as deprecated
    The CastToClasses class has been marked as deprecated with its functionality replaced by the ArrayOf class. This refactoring simplifies the parse function by eliminating the need for explicit looping and casting. Developers are encouraged to use the ArrayOf class moving forward.

--- src/Attributes/CastToClasses.php
+++ src/Attributes/CastToClasses.php
@@ -9,6 +9,9 @@ namespace Zerotoprod\ServiceModel\Attributes;
 use Attribute;
 use Zerotoprod\ServiceModel\Contracts\CanParse;
 
+/**
+ * @deprecated use ArrayOf instead
+ */
 #[Attribute]
 class CastToClasses implements CanParse
 {
@@ -18,25 +21,6 @@ class CastToClasses implements CanParse
 
     public function parse(array $values): array
     {
-        $results = [];
-
-        foreach ($values as $value) {
-            if (is_array($value)) {
-                $results[] = new $this->class(...$value);
-                continue;
-            }
-
-            if ((is_int($value) || is_string($value))
-                && enum_exists($this->class)
-                && method_exists($this->class, 'tryFrom')
-            ) {
-                $results[] = $this->class::tryFrom($value);
-                continue;
-            }
-
-            $results[] = new $this->class($value);
-        }
-
-        return $results;
+        return (new ArrayOf($this->class))->parse($values);
     }
 }
