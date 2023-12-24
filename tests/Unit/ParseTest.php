71fc33862ad0055dd1801867db9d87119e5b54b6
david_smith <david_smith@sweetwater.com>
2023-12-23 22:00:30 -0500

    Update ParseTest to use CastUsingClass
    Revised the ParseTest class to use 'CastUsingClass' instead of the 'CastMethodClass.' The change ensures more logical naming and improved code readability. This setup may increase the specificity of the testing process.

--- tests/Unit/ParseTest.php
+++ tests/Unit/ParseTest.php
@@ -1,10 +1,10 @@
 <?php
 
-use Zerotoprod\AppServiceModel\Tests\Models\CastMethodClass;
+use Zerotoprod\AppServiceModel\Tests\Models\CastUsingClass;
 use Zerotoprod\AppServiceModel\Tests\Models\TimeClass;
 
 test('time value is cast to TimeClass instance', function () {
-    expect(CastMethodClass::make([CastMethodClass::time => '2021-01-01 00:00:00'])
+    expect(CastUsingClass::make([CastUsingClass::time => '2021-01-01 00:00:00'])
         ->time
     )->toBeInstanceOf(TimeClass::class);
 });
