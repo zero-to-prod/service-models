3fe21f2c87cfb8ddf6da75594f3d40345f24216c
david_smith <david_smith@sweetwater.com>
2023-12-23 21:52:48 -0500

    Add CastUsingTest unit test
    A new unit test file, CastUsingTest.php, was created. This test verifies the correct casting functionality of the CastUsingClass. It checks if the "value" and "values" attributes are being instantiated as instances of "SpecialCast" and "SpecialCastValues" respectively, and whether the content within these objects matches the expected output.

---
+++ tests/Unit/CastUsingTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Zerotoprod\AppServiceModel\Tests\Models\CastUsingClass;
+use Zerotoprod\AppServiceModel\Tests\Models\SpecialCast;
+use Zerotoprod\AppServiceModel\Tests\Models\SpecialCastValues;
+
+test('cast using', function () {
+    $CastUsingClass = CastUsingClass::make([
+        CastUsingClass::value => 'value',
+        CastUsingClass::values => [
+            'id' => 'id',
+            'name' => 'name'
+        ],
+    ]);
+    expect($CastUsingClass->value)->toBeInstanceOf(SpecialCast::class)
+        ->and($CastUsingClass->value->value)->toBe('value')
+        ->and($CastUsingClass->values)->toBeInstanceOf(SpecialCastValues::class)
+        ->and($CastUsingClass->values->id)->toBe('id')
+        ->and($CastUsingClass->values->name)->toBe('name');
+});
