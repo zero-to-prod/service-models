3be292cc6d2042a72f58a7f0d83d2fee9915b7a0
david_smith <david_smith@sweetwater.com>
2023-12-23 21:52:32 -0500

    Rename CastMethodClass and update attribute usage in tests
    The CastMethodClass was renamed to CastUsingClass and its attribute usage was updated. Specifically, all references to the 'CastMethod' attribute were replaced with 'CastUsing'. The changes reflect the class and attribute's functionalities better, improving overall code readability.

--- tests/Models/CastUsingClass.php
+++ tests/Models/CastUsingClass.php
@@ -2,17 +2,20 @@
 
 namespace Zerotoprod\AppServiceModel\Tests\Models;
 
-use Zerotoprod\ServiceModel\Attributes\CastMethod;
+use Zerotoprod\ServiceModel\Attributes\CastUsing;
 use Zerotoprod\ServiceModel\ServiceModel;
 
-class CastMethodClass
+class CastUsingClass
 {
     use ServiceModel;
 
     public const value = 'value';
+    public const values = 'values';
     public const time = 'time';
 
-    #[CastMethod('set')]
+    #[CastUsing('set')]
     public SpecialCast $value;
+    #[CastUsing('set')]
+    public SpecialCastValues $values;
     public TimeClass $time;
 }
