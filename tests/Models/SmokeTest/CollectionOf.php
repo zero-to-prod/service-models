f6f3dcc48208b9c10f52080100291c3478a3aef3
david_smith <david_smith@sweetwater.com>
2023-12-23 21:54:08 -0500

    Add CollectionOf class in SmokeTest namespace
    The CollectionOf class has been introduced in the 'SmokeTest' namespace. This class, which implements the 'CanParse' interface, is designed to create a collection from an array of values. The added functionality will enhance testing capabilities, particularly for smoke testing scenarios.

---
+++ tests/Models/SmokeTest/CollectionOf.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Zerotoprod\AppServiceModel\Tests\Models\SmokeTest;
+
+use Attribute;
+use Zerotoprod\ServiceModel\Contracts\CanParse;
+
+#[Attribute]
+class CollectionOf implements CanParse
+{
+    public function __construct(public readonly string $class)
+    {
+    }
+
+    public function parse(array $values): Collection
+    {
+        return new Collection(array_map(fn(array $item) => $this->class::make($item), $values));
+    }
+}
