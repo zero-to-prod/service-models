e71fe47655f31147046ce3d272a5e2fb244e273e
david_smith <david_smith@sweetwater.com>
2024-07-29 15:52:53 -0400

    Ignore attributes that don't implement parse method.

---
+++ tests/Casters/CustomAttribute.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @noinspection PhpUndefinedClassInspection
+ * @noinspection PhpUndefinedFunctionInspection
+ */
+
+namespace Zerotoprod\AppServiceModel\Tests\Casters;
+
+use Attribute;
+use Zerotoprod\ServiceModel\Contracts\CanParse;
+
+#[Attribute]
+class CustomAttribute
+{
+    public function __construct(public readonly int $value)
+    {
+    }
+}
