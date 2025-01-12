77f14677b4a51fd9ca018f7e5a07c854e436de60
david_smith <david_smith@sweetwater.com>
2025-01-12 17:02:19 -0500

    maint/dock Created `test.sh`.

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+PHP_VERSIONS=("8.4" "8.3" "8.2" "8.1")
+
+for PHP_VERSION in "${PHP_VERSIONS[@]}"; do
+
+  sh dock "composer${PHP_VERSION}" composer update
+
+  if ! sh dock "php${PHP_VERSION}" ./vendor/bin/pest
+  then
+    exit 1
+  fi
+done
